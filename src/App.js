86b5ecc323396cf90a3f5baa038d81e24ca6c2c1
Emil Karlsson <emil.karl@gmail.com>
2018-09-23 11:50:41 +0200

    First commit with copy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./assets/images/coinid-logo.png";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Myriad Wallet for COINiD</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          If the goals are met we will add support for Myriad in COINiD.
         </p>
       </div>
     );
